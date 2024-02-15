f0ad9e2e0ef69cd83520b3c4900035192ee608d0
LidianeCarv <lidigugasgb@gmail.com>
2024-02-15 12:24:42 -0300

    Commit Inicial

---
+++ src/main/java/com/interesseService/encontrarLeiloes/EncontrarLeiloesApplication.java
@@ -0,0 +1,13 @@
+package com.interesseService.encontrarLeiloes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncontrarLeiloesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncontrarLeiloesApplication.class, args);
+	}
+
+}
